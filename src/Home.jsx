59cb65f7926cd1f01d579bea0f2fac4adffc25bc
Nathan <natspectro@gmail.com>
2023-02-21 00:04:25 +0100

    Created db.json

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Bloglist from "./BlogList";
 
 const Home = () => {
@@ -7,13 +7,22 @@ const Home = () => {
     { title: "Welcome party!", body: "lorem ipsum...", author: "Kayla", id: 2 },
     { title: "Web dev tips", body: "lorem ipsum...", author: "Nate", id: 3 },
   ]);
+  const [name, setName] = useState("Nathan");
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("Use Effect Ran");
+    console.log(name);
+  }, [name]);
+
   return (
     <div className="home">
       <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName("Janice")}>Change Name</button>
+      <h3>{name}</h3>
     </div>
   );
 };
