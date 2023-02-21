5e4be6bc98785c59ea52f84bd87a20e1f4981748
Nathan <natspectro@gmail.com>
2023-02-21 10:11:33 +0100

    Added fetch functionality. Removed delete functionality. Added Conditional templating for Bloglist rendering.

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,27 +2,22 @@ import { useState, useEffect } from "react";
 import Bloglist from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "Nate", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "Kayla", id: 2 },
-    { title: "Web dev tips", body: "lorem ipsum...", author: "Nate", id: 3 },
-  ]);
-  const [name, setName] = useState("Nathan");
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log("Use Effect Ran");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName("Janice")}>Change Name</button>
-      <h3>{name}</h3>
+      {blogs && <Bloglist blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
