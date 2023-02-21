d69adc481454ae92fbea8eb8b7eb987e581947fa
Nathan <natspectro@gmail.com>
2023-02-21 12:06:27 +0100

    Added abort fetch functionality (try to fix more later)

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -4,10 +4,10 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
+    const abortCont = new AbortController();
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
-          console.log(res);
           if (!res.ok) {
             throw Error("Could not fetch Data!");
           }
@@ -19,10 +19,15 @@ const useFetch = (url) => {
           setError(null);
         })
         .catch((err) => {
-          setError(err.message);
-          setIsPending(false);
+          if (err.name === "AbortError") {
+            console.log("No fetch");
+          } else {
+            setError(err.message);
+            setIsPending(false);
+          }
         });
     }, 1000);
+    return () => abortCont.abort;
   }, [url]);
 
   return {
